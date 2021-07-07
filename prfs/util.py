29096bcc413f6f623f8e40d210445c5bdd9bacb2
Rory Kurek <rorykurek@gmail.com>
2021-07-07 16:46:36 -0600

    Reorganize reliefrate.py

--- prfs/util.py
+++ prfs/util.py
@@ -1,5 +1,8 @@
+from scipy.optimize import root_scalar
+from functools import cache
 import thermo as th
 import thermo.interaction_parameters as ip
+from . import ureg, Q_
 
 
 def create_VL_flasher(names: list[str]) -> th.flash.FlashVL:
@@ -20,3 +23,21 @@ def create_VL_flasher(names: list[str]) -> th.flash.FlashVL:
 
     # Create VL flasher
     return th.FlashVL(constants, properties, liquid=liquid, gas=gas)
+
+
+@ureg.wraps(None, (None, ureg.Pa, ureg.dimensionless, None))
+@cache
+def flash_to_VF(flasher: th.flash.Flash, P: Q_, VF: Q_, zs) \
+        -> th.equilibrium.EquilibriumState:
+
+    if VF == 0.0 or VF == 1.0:
+        return flasher.flash(P=P, VF=VF, zs=zs)
+    else:
+        sat_liquid = flash_to_VF(flasher, P=Q_(P, 'Pa'), VF=Q_('0.0'), zs=zs)
+        sat_vapor = flash_to_VF(flasher, P=Q_(P, 'Pa'), VF=Q_('1.0'), zs=zs)
+
+        def check_T(T_val):
+            return flasher.flash(T=T_val, P=P, zs=zs).VF - VF
+
+        T = root_scalar(check_T, bracket=[sat_liquid.T, sat_vapor.T]).root
+        return flasher.flash(T=T, P=P, zs=zs)
